7c0617cc259a54985ed8490aef879b3375f85f3d
kesavan <kesavanplk7@gmail.com>
2023-05-19 11:59:52 +0530

    add needed components

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,23 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover&share
+        <span className="orange_gradient text-center">AI-powered prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </p>
+    </section>
+  );
 };
 
 export default Home;
